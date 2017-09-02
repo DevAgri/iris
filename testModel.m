38289f4a2f3b72f089e2db9cdfdc8621ddfc78a3
Duarte <william.2025@hotmail.com>
2017-09-02 16:34:46 -0400

    first

--- testModel.m
+++ testModel.m
@@ -3,9 +3,9 @@ close all;
 clc
 pkg load statistics
 %% Parametros da rede
-input_layer_size  = 400;  % imagens de 20x20 
-hidden_layer_size = 25;   % 25 unidades na camada intermediária
-num_labels = 10;          % 10 classe, de 1 a 10
+input_layer_size  = 30000;  % imagens de 20x20 
+hidden_layer_size = 1000;   % 25 unidades na camada intermediária
+num_labels = 2;          % 10 classe, de 1 a 10
 
 k=3;
 load('ex5data.mat');
@@ -20,7 +20,7 @@ for i=1:k,
 fprintf('\nTreinando a rede neural... \n')
 
 
-options = optimset('MaxIter', 400);
+options = optimset('MaxIter', 50);
 
 %  should also try different values of lambda
 lambda = 1;

12854e4f69682cfb482c045905292054e66e5159
Duarte <william.2025@hotmail.com>
2017-09-02 02:58:09 -0400

    first

---
+++ CREATEDATABASE.m
@@ -0,0 +1,51 @@
+clear 
+clc
+pkg load image
+dirClassOne = './queimados/' ;
+dirClassZero = './bons/';
+
+addpath('./libsvm-3.22/matlab');
+filesClassOne = dir(strcat([dirClassOne, '*.jpg'])) ;
+m1 = size(filesClassOne, 1);
+
+filesClassZero = dir(strcat([dirClassZero, '*.jpg'])) ;
+m0 = size(filesClassZero, 1);
+X = zeros(m0+m1, 30000);
+for i=1:m1,
+
+        fprintf('%f\n',  100*i/m1);
+
+        filename = char(filesClassOne(i).name);
+        
+        im = double(imread(strcat([dirClassOne filename])));
+        %figure, imshow(im);
+        im = im/255;
+        %figure, imshow(im);
+        im= imresize(im, [100 100]);
+        %figure,imshow(im);
+        X(i,:) = im(:);
+        
+end
+
+for i=1:m0,
+
+        fprintf('%f\n',  100*(i/m0));
+
+        filename = char(filesClassZero(i).name);
+        im = double(imread(strcat([dirClassZero filename])));
+        %figure, imshow(im);
+        im = im/255;
+        im =  imresize(im, [100 100]);
+        %figure, imshow(im);
+        X(i+m1,:) = im(:);
+        
+end
+
+y = zeros(m0+m1, 1);
+y(1:m1) = 1;
+y(m1+1:m1+m0) = 2;
+
+features_sparse = sparse(X);
+
+libsvmwrite('parkData.txt', y, features_sparse); 
+save('parkSlotData1.mat', 'X', 'y');
---
+++ aux_1.m
@@ -0,0 +1,75 @@
+function [J,  grad] = cost_function(nn_params, ...
+                                   input_layer_size, ...
+                                   hidden_layer_size, ...
+                                   num_labels, ...
+                                   X, y, lambda)
+%COST_FUNCTION implementa a função de custo da rede neural
+%   [J grad] = COST_FUNCTON(nn_params, hidden_layer_size, num_labels, ...
+%   X, y, lambda) calcula a função de custo  e o gradiente da rede. 
+%
+
+% Não altere!!
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+
+% Variáveis úteis
+m = size(X, 1);
+
+% Você deve preencher as seguintes variáveis corretamente
+J = 0;
+Theta1_grad = zeros(size(Theta1)); % gradiente de Theta1
+Theta2_grad = zeros(size(Theta2)); % gradiente de Theta2
+
+
+% Mudança de representação de y para um vetor Y
+I = eye(num_labels);
+Y = zeros(m, num_labels);
+for i=1:m
+  Y(i, :)= I(y(i), :);
+end
+%==========FUNÇÃO DE CUSTO============
+for i=1:m,
+     a_1=X(i,:);
+     a_1=[1,a_1];
+     z_2=a_1*Theta1';
+     a_2=sigmoid(z_2);
+     a_2=[1,a_2];
+     z_3=a_2*Theta2';
+     a_3=sigmoid(z_3);
+     J=J+(-1)*sum(Y(i,:).*log(a_3))-sum((1-Y(i, :)).*log(1-a_3));
+     end
+     J=J/m;
+    J=J+lambda/(2*m)*(sum(sum(Theta1.*Theta1)')+sum(sum(Theta2.*Theta2)));
+    % =============== Sua implementação deve ser vir aqui ==================
+a_1=X(1,:);
+a_1=[1,a_1];
+z_2=a_1*Theta1';
+a_2=sigmoid(z_2);
+a_2=[1,a_2];
+z_3=a_2*Theta2';
+a_3=sigmoid(z_3);
+r_3=a_3-y(1);
+r_2=(Theta2'*r_3').*[1,sigmoidGradient(z_2)]';;
+delta_1=r_2*a_1;
+delta_2=r_3'*a_2;
+for i=2:m,
+    a_1=X(i,:);
+    a_1=[1,a_1];
+    z_2=a_1*Theta1';
+    a_2=sigmoid(z_2);
+    a_2=[1,a_2];
+    z_3=a_2*Theta2';
+    a_3=sigmoid(z_3);
+    r_3=a_3-Y(i);
+    r_2=(Theta2'*r_3').*[1,sigmoidGradient(z_2)]';
+    delta_1=r_2*a_1;
+    delta_2=r_3'*a_2;
+end
+Theta1_grad=(1/m)*delta_1(2:end, :)+(lambda/m)*Theta1;
+Theta2_grad=(1/m)*delta_2+(lambda/m)*Theta2;
+% Não altere esta linha!!
+grad = [Theta1_grad(:) ; Theta2_grad(:)];
+end
